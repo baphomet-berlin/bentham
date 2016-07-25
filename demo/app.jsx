24bc2da4ba5fbea80947419153c9b889fca885e7
Claudio Brandolino <cbrandolino@gmail.com>
2016-07-25 04:35:29 +0200

    Added basic pie chart to examples

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,12 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryPie } from 'victory';
+import Playground from 'component-playground';
+
+const componentExample = require("raw!./examples/DonutChart.example");
 
 class Index extends React.Component {
   render() {
-    return <div />
+    return (
+      <div className="component-documentation">
+        <Playground codeText={componentExample} scope={{React: React, Pie: VictoryPie}}/>
+      </div>
+    );
   }
 }
-
-
 
 ReactDOM.render(<Index/>, document.getElementById("content"));
