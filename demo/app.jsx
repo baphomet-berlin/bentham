9ddf3ec06787c340d31ea5b205ada26c5786bac9
Claudio Brandolino <cbrandolino@gmail.com>
2016-07-25 05:44:02 +0200

    Added some style

--- demo/app.jsx
+++ demo/app.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { VictoryPie } from 'victory';
 import Playground from 'component-playground';
+import { Container, Row, Col } from 'reactstrap';
 
 const componentExample = require("raw!./examples/DonutChart.example");
 
 class Index extends React.Component {
   render() {
-    return (
-      <div className="component-documentation">
-        <Playground codeText={componentExample} scope={{React: React, Pie: VictoryPie}}/>
-      </div>
+    return (          
+      <Playground codeText={componentExample} scope={{React: React, Pie: VictoryPie}} />
     );
   }
 }
